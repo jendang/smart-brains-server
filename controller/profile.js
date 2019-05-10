ca8876e99283575b5c3a68a44ade20fa83d8694d
jennydang <jenny.dang117@gmail.com>
2019-05-10 22:25:27 +0200

    factoring code

---
+++ controller/profile.js
@@ -0,0 +1,19 @@
+const handleUserProfile = (req, res, db) => {
+    const  {id}  = req.params
+    db
+        .select('*')
+        .where({id})
+        .from('users')
+        .then(user => {
+            if(user.length){
+                res.json(user[0])
+            }else {
+                res.status(404).json("user not found")
+            }
+        })
+        .catch(err => res.status(400).json(err))
+}
+
+module.exports = {
+    handleUserProfile: handleUserProfile
+}
